0fbf241ad17ec29568bb598d5b6fee8e7db88f5c
Seba Cardoso <hi@sebastiancardoso.com>
2023-08-16 00:51:13 -0300

    👋

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,8 @@ export default component$(() => {
   return (
     <main class="flex-grow flex items-center justify-center">
       <div class="flex flex-col items-center">
-        <img src="/images/me.webp" alt="Foto de sebastian" class="rounded-full w-32 h-32 object-cover shadow-lg my-4" />
+        <img src="/images/me.webp" alt="Foto de sebastian" class="rounded-full w-32 h-32 object-cover border border-blue-500 shadow-xl my-4" />
+        <div class="mt-4 text-4xl animated jello">👋</div>
         <div class="text-3xl font-bold my-2">Sebastián Cardoso</div>
         {/* <div class="text-lg my-2">Web developer</div> */}
         <div class="typewriter-effect">{t('home.description@@Hello! I\'m Frontend developer.')}</div>
